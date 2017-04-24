5fe2c40021b14511f403593e96adb5a05c50d2fc
Mario Medina <mario@opticalcube.com>
2017-04-23 19:48:26 -0500

    Basic construction of protocol and data structures

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  label1 = 'alsjdfl';
+
+  txs = [
+    {id: 1, amount: 1.234 },
+    {id: 2, amount: 0.234 },
+    {id: 3, amount: 5.123 },
+    {id: 4, amount: 9.000 },
+  ]
 
   constructor(public navCtrl: NavController) {
 
